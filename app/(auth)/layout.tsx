8a44eec92627cbe06ee67ffe6ecac0b6c0aba559
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-12-05 13:12:06 +0700

    Prettier format code

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='h-full flex items-center justify-center'>{children}</div>
-  );
-};
+  return <div className="flex h-full items-center justify-center">{children}</div>
+}
 
-export default AuthLayout;
+export default AuthLayout
